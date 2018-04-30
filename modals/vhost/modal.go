495490ee449abcd41e6f34d5bde384b2288bd15e
Tony Yip <tony@opensource.hk>
2018-04-30 20:04:39 +0800

    Add store for vhost modal

---
+++ modals/vhost/modal.go
@@ -0,0 +1,7 @@
+package vhost
+
+type VirtualHost struct {
+	Hostname    string
+	OauthID     string
+	OAuthSecret string
+}
---
+++ modals/vhost/store.go
@@ -0,0 +1,32 @@
+package vhost
+
+import (
+	"context"
+	"database/sql"
+
+	"golang.ysitd.cloud/db"
+)
+
+type Store struct {
+	DB *db.GeneralOpener `inject:""`
+}
+
+func (s *Store) GetVHost(ctx context.Context, hostname string) (host *VirtualHost, err error) {
+	conn, err := s.DB.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	query := "SELECT hostname, oauth_id, oauth_secret FROM host WHERE hostname = $1"
+	row := conn.QueryRowContext(ctx, query, hostname)
+
+	var instance VirtualHost
+	if err := row.Scan(&instance.Hostname, &instance.OauthID, &instance.OAuthSecret); err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &instance, nil
+}
