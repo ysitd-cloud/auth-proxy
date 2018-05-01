f3b1b11187bc059dc1bbf93b267f18a381df88cf
Tony Yip <tony@opensource.hk>
2018-05-01 20:55:01 +0800

    Wrap handler with gorilla handlers

--- handler/mux.go
+++ handler/mux.go
@@ -1,24 +1,39 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/handlers"
+
+	"golang.ysitd.cloud/log"
+)
 
 type MainHandler struct {
-	mux             *http.ServeMux
+	http.Handler
+	Logger          log.Logger       `inject:"main logger"`
 	CallbackHandler *CallbackHandler `inject:""`
 	LoginHandler    *LoginHandler    `inject:""`
 	Proxy           *Proxy           `inject:""`
 }
 
 func (h *MainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h.mux == nil {
+	if h.Handler == nil {
 		h.setupMux()
 	}
-	h.mux.ServeHTTP(w, r)
+	h.Handler.ServeHTTP(w, r)
 }
 
 func (h *MainHandler) setupMux() {
-	h.mux = http.NewServeMux()
-	h.mux.Handle("/", h.Proxy)
-	h.mux.Handle("/auth/ysitd", h.LoginHandler)
-	h.mux.Handle("/auth/ysitd/callback", h.CallbackHandler)
+	mux := http.NewServeMux()
+	mux.Handle("/", h.Proxy)
+	mux.Handle("/auth/ysitd", h.LoginHandler)
+	mux.Handle("/auth/ysitd/callback", h.CallbackHandler)
+
+	httpLogger := h.Logger.WithField("source", "http")
+	handler := handlers.CombinedLoggingHandler(httpLogger.Writer(), mux)
+	logOpt := handlers.RecoveryLogger(h.Logger.WithField("source", "recover"))
+	printOpt := handlers.PrintRecoveryStack(true)
+	handler = handlers.RecoveryHandler(logOpt, printOpt)(handler)
+
+	h.Handler = handler
 }
